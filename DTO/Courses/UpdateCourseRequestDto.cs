f1b04bb972c1e6c477d2426f570909580263ee57
Eman_Shehata <emanshehata258@gmail.com>
2026-01-10 22:24:49 +0200

    make courses's apis

---
+++ DTO/Courses/UpdateCourseRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineExaminationSystem.DTO.Courses
+{
+    public class UpdateCourseRequestDto
+    {
+        public int? TrackId { get; set; }
+        public string? CourseCode { get; set; }
+        public string? CourseName { get; set; }
+        public string? Description { get; set; }
+    }
+}
